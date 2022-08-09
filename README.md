bc49bc8e9395d8611b276318e6003ba48c3070b1
Justin Hedrick <Justinchedrick@gmail.com>
2022-08-09 16:35:22 -0700

    updated auth for non-users, homepage renders with options to login/sign up/nav bar is working as well

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 // A marketplace for potential grant applicants
 
-//Test
+// 8/9/2022 - Justin
+// Converted site from first page being the login/sign up auth form to non-users being redirected to home page first
+// updated nav bar with Home|signup|login
+// App.js directs to homepage for non-user
 
-//Test Tesh
+// TODO 8/9/2022
+// Focus on styling home page correctly and added additional pages and routes to match wireframes
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,11 +12,24 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-     <Link to="/profile"><span>{user.name}</span></Link>
-      &nbsp; | &nbsp;
-     <Link to="/">Home</Link>
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
+      {user ? 
+      <>
+        <Link to="/profile"><span>{user.name}</span></Link>
+        &nbsp; | &nbsp;
+        <Link to="/home">Home</Link>
+        &nbsp; | &nbsp;
+        <Link to="" onClick={handleLogOut}>Log Out</Link>
+      </>
+      :
+      // added functionality for non-users to see a nav bar
+      <>
+        <Link to="/home">Home</Link>
+        &nbsp; | &nbsp;
+        <Link to="/sign-up">Sign-up</Link>
+        &nbsp; | &nbsp;
+        <Link to="/login">Login</Link>
+      </>
+      } 
     </nav>
   );
 }
