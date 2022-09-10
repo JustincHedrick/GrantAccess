04ea3139bd99b19a700ea804ca810cda60f83f5c
Justin Hedrick <Justinchedrick@gmail.com>
2022-09-09 21:49:08 -0700

    deploy

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -6,11 +6,7 @@ import Message from '../../components/Message/Message';
 import Online from '../../components/Online/Online'
 import axios from 'axios';
 import {io} from 'socket.io-client'
-
-
-
 export default function Chat({user, grants}) {
-
   const [conversations, setConversations] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
@@ -19,7 +15,7 @@ export default function Chat({user, grants}) {
   const [onlineUsers, setOnlineUsers] = useState([]);
   const socket = useRef(io("http://localhost:3002"))
   const scrollRef = useRef()
-
+  
   useEffect(() => {
     socket.current = io("ws://localhost:3002");
     socket.current.on("getMessage", (data) => {
@@ -30,7 +26,7 @@ export default function Chat({user, grants}) {
       });
     });
   }, [user._id, socket, currentChat]);
-  
+
   useEffect(() => {
     arrivalMessage &&
       currentChat?.members.includes(arrivalMessage.sender) &&
@@ -75,17 +71,14 @@ export default function Chat({user, grants}) {
       text : newMessage,
       conversationId: currentChat._id,
     }
-
     const receiverId = currentChat.members.find(
       (member) => member !== user._id
     );
-
     socket.current.emit("sendMessage", {
       senderId: user._id,
       receiverId,
       text: newMessage
     })
-
     try {
       const res = await axios.post("/api/messages", message );
       setMessages([...messages, res.data]);
@@ -94,46 +87,41 @@ export default function Chat({user, grants}) {
       console.log(err);
     }
   }
-
   useEffect(() => {
     scrollRef.current?.scrollIntoView({behavior: "smooth",  block: 'nearest'})
   }, [messages])
-
   return(
     <>
       <div className="messenger flex flex-row h-[80vh]">
         <div className="chatMenu basis-1/5 text-white flex h-full">
           <div className="p-6 bg-emerald-900 w-full">
             <input type="search" placeholder='Search for mentors' class="text-black w-11/12 py-2.5 border-none"></input>
-              
                 {conversations.map(c => (
-                  <div onClick={() => setCurrentChat(c)} > 
+                  <div onClick={() => setCurrentChat(c)} >
                     <Conversation user={user} conversations={c} />
                   </div>
                 ))}
-              
           </div>
         </div>
         <div className="chatBox basis-2/5 text-white flex h-full w-full">
           <div className="p-6 bg-neutral-200 flex flex-col w-full relative">
-            { currentChat ? 
+            { currentChat ?
             <>
             <div className="chatBoxTop h-full overflow-y-auto pr-2">
-              {messages.map(m => 
+              {messages.map(m =>
                 <div ref={scrollRef}>
-                  <Message message={m} own={m.sender === user._id}/>  
+                  <Message message={m} own={m.sender === user._id}/>
                 </div>
               )}
             </div>
             <div className="chatBoxBottom mt-1.5 flex items-center justify-between">
-              <textarea 
-              placeholder='Write something...' 
+              <textarea
+              placeholder='Write something...'
               className='w-4/5 h-24 p-2 text-black'
               onChange={(e) => setNewMessage(e.target.value)}
               value={newMessage}>
-
               </textarea>
-              <button 
+              <button
               className='h-10 w-16 rounded border-none bg-emerald-900 cursor-pointer'
               onClick={handleSubmit}>
                 Send
