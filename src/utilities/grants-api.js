be326579ecdaeaedccd966f17d16c43d517b052f
Roger Davila <rogerddavila@gmail.com>
2022-09-10 19:29:42 -0700

    add save grants feature

--- src/utilities/grants-api.js
+++ src/utilities/grants-api.js
@@ -4,6 +4,10 @@ const BASE_URL = "/api/grants";
 
 // GET
 export function getGrants() {
-  return sendRequest(`${BASE_URL}`)
+  return sendRequest(`${BASE_URL}`);
 }
 
+// PUT - Used when updating save/unsaved grants
+export function saveGrant(payload) {
+  return sendRequest(`${BASE_URL}/grant`, 'PUT', payload);
+}
