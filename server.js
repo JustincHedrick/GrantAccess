3fcab076ff085ce5a8a4da7d84caa2bf785d93b2
Justin Hedrick <Justinchedrick@gmail.com>
2022-09-11 02:34:14 -0700

    RESET to when local worked with add ons for http server creation and correct origins

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express');
-const cors = require('cors');
+var cors = require('cors');
 const app = express();
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const port = process.env.PORT || 3001;
 const http = require('http').createServer(app);
-
-app.use(cors());
-
 const io = require("socket.io")(http, {
   cors: {
-    origin: ['https://grantguide.herokuapp.com/'],
-    methods: ["GET", "POST"],
+    origin: "https://grantguide.herokuapp.com/",
+    method: ['POST', 'GET']
   }
 });
 
 require('dotenv').config();
 // Connect to db after the dotenv above
 require('./config/database');
-app.use(logger('dev'));
 
-// Process data in body of request if
+app.use(logger('dev'));
+// Process data in body of request if 
 // Content-Type: 'application/json'
 // and put that data on req.body
 
@@ -29,7 +25,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
-
 
 // Middleware to verify token and assign user object of payload to req.user.
 // Be sure to mount before routes
@@ -40,9 +35,9 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/grants', require('./routes/api/grants'));
 app.use('/api/conversations', require('./routes/api/conversations'));
 app.use('/api/messages', require('./routes/api/messages'));
+
 // "catch-all" route that will match all GET requests
 // that don't match an API route defined above
-
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
@@ -53,54 +48,60 @@ const addUser = (userId, socketId) => {
   !users.some(user=> user.userId === userId) && users.push({ userId, socketId })
   console.log(users)
 }
+
 const removeUser = (socketId) => {
   users = users.filter((users) => users.socketId !== socketId)
 }
 
 const getUser = (userId) => {
+  console.log(userId)
   return users.find((user) => user.userId === userId);
 };
 
 io.on("connection", (socket) => {
   //when ceonnect
+  console.log("a user connected.");
   //take userId and socketId from user
-  console.log('on')
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
-    console.log(users)
     io.emit("getUsers", users);
   });
 
   //send and get message
+
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    //send message when both sender and reciever are connected
+    console.log(receiverId, senderId)
     if (users.some((user) => user.userId === receiverId)) {
       const user = getUser(receiverId);
       io.to(user.socketId).emit("getMessage", {
       senderId,
       text,
-    });
+    }); 
     } else {
-      // send message when only sender is connected to socket
       socket.on("sendMessage", ({ senderId, receiverId, text }) => {
       const user = getUser(senderId);
       io.to(user.socketId).emit("getMessage", {
       senderId,
       text,
-    });
+    }); 
   });
     }
+      
   });
+    
+      
 
   //when disconnect
   socket.on("disconnect", () => {
+    console.log("a user disconnected!");
     removeUser(socket.id);
     io.emit("getUsers", users);
   });
 });
 
-
+const port = process.env.PORT || 3001;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`);
 });
+
