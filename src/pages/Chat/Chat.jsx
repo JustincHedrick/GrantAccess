0d70cfe3b43f3fac34c5dfa1c318179848d97253
Justin Hedrick <Justinchedrick@gmail.com>
2022-09-10 23:25:41 -0700

    grants update - state updates on each grant save

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -15,7 +15,7 @@ export default function Chat({user, grants}) {
   const [arrivalMessage, setArrivalMessage] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState([]);
   const endpoint = 'https://grantguide.herokuapp.com/';
-  const socket = io(endpoint)
+  const socket = useRef()
   const scrollRef = useRef()
   
   useEffect(() => {
