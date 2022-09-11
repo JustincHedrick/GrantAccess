82175ff97cd645636d5e776e41c083d0ea22bd4c
Justin Hedrick <Justinchedrick@gmail.com>
2022-09-11 00:07:44 -0700

    broke the app, switching server code back

--- server.js
+++ server.js
@@ -99,10 +99,6 @@ io.on("connection", (socket) => {
 
 const port = process.env.PORT || 3001;
 
-http.listen(port,function(){
-    console.log("Listening to port " + port);
-});
-
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`);
 });
