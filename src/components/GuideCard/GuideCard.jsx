23c2e5078adb02d802325c33599e9bf71be30b64
Justin Hedrick <Justinchedrick@gmail.com>
2022-09-14 12:12:25 -0700

    final final final final final

--- src/components/GuideCard/GuideCard.jsx
+++ src/components/GuideCard/GuideCard.jsx
@@ -8,45 +8,42 @@ export default function GuideCard({guides, user, guideId}) {
   
   const [guideInfo, setGuideInfo] = useState(guides);
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   // pass data to controller/api
-  async function newConversation() {
-    let data = {
-      senderId: user._id,
-      receiverId: guides._id,
-    } ;
-    await conversationApi.newConversation(data)
-  }
+  // async function newConversation() {
+  //   let data = {
+  //     senderId: user._id,
+  //     receiverId: guides._id,
+  //   } ;
+  //   await conversationApi.newConversation(data)
+  // }
 
-  useEffect(() => {
-
-  }, [])
 
   function getGuideId() {
     let guide = guides._id
     setGuideInfo(guide)
   }
 
-  function handleClick() {
-    navigate('/chat')
-  };
+  // function handleClick() {
+  //   navigate('/chat')
+  // };
   
   
   return(
     <> 
-      {/* <Link to="/guideprofile" onClick={getGuideId} state={{guide: guideInfo}}> */}
+      <Link to="/guideprofile" onClick={getGuideId} state={{guide: guideInfo}}>
       <div className="flex flex-col justify-end h-52 w-4/6 rounded-md bg-slate-100 border border-slate-400 mt-20 pl-7">
         
         <div className='flex flex-row justify-between' value={guideId}>
           <img class="mb-3 w-24 h-24 rounded-full shadow-lg" src="https://source.unsplash.com/75x75/?portrait" alt="Guide image"/>
-          <button className='h-8 w-36 bg-primary-50 text-xs rounded mt-16 mr-10 font-semibold hover:bg-primary-10' onClick={(e) =>  {newConversation(); handleClick();}}>Connect with {guides.firstName}</button>
+          {/* <button className='h-8 w-36 bg-primary-50 text-xs rounded mt-16 mr-10 font-semibold hover:bg-primary-10' onClick={(e) =>  {newConversation(); handleClick();}}>Connect with {guides.firstName}</button> */}
         </div>
         <span className="font-bold text-xl my-1">{guides.firstName + ` ` + guides.lastName}</span>
         <span className="my-1 text-lg">{guides.role + ` @ ` + guides.organization}</span>
         <span className="text-xs text-slate-400 my-1">{guides.location}</span>
         <span className="my-1">{guides.about}</span>
       </div>
-      {/* </Link> */}
+      </Link>
     </>
   )
 }
--- src/components/MessageButton/MessageButton.jsx
+++ src/components/MessageButton/MessageButton.jsx
@@ -12,12 +12,12 @@ export default function MessageButton() {
         </button>
       </Link>
       {/* Alert */}
-      <button className="inline-block relative">
+      {/* <button className="inline-block relative">
         <svg xmlns="http://www.w3.org/2000/svg" className='general-icon' fill="none" viewBox="0 0 24 24">
           <path className="stroke-primary-700" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
         </svg>
         <span class="animate-ping absolute top-1 right-0.5 block h-1 w-1 rounded-full ring-2 ring-green-400 bg-green-600"></span>
-      </button>
+      </button> */}
     </div>
   )
 }
